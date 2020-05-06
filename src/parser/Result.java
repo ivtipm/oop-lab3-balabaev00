e3b50cbbe941aa75b3510a2122296a68423783f2
balabaev00 <balabaev00@yandex.ru>
2020-05-06 11:58:42 +0900

    add project

---
+++ src/parser/Result.java
@@ -0,0 +1,14 @@
+package parser;
+
+public class Result
+{
+
+    public double acc;
+    public String rest;
+
+    public Result(double v, String r)
+    {
+        this.acc = v;
+        this.rest = r;
+    }
+}
